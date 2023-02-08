572318b7bf4a716f486a7aaacce148a2190c39db
guo_yugang <unis_guoyugang@47.92.227.67:10010>
2023-02-08 16:25:41 +0800

    内容放到vue组件中

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,21 @@
 <template>
   <div class="hello">
+    <a-scene arjs embedded renderer="logarithmicDepthBuffer: true;" vr-mode-ui="enabled: false" gesture-detector
+    	id="scene">
+    	<a-assets>
+    		<a-asset-item id="bowser"
+    			src="https://cdn.glitch.com/06bd98b4-97ee-4c07-a546-fe39ca205034%2Fbowser.glb">
+    		</a-asset-item>
+    	</a-assets>
     
+    	<a-marker preset="hiro" raycaster="objects: .clickable" emitevents="true"
+    		cursor="fuse: false; rayOrigin: mouse;" id="markerA">
+    		<a-entity id="bowser-model" gltf-model="#bowser" position="0 0 0" scale="0.05 0.05 0.05"
+    			class="clickable" gesture-handler>
+    		</a-entity>
+    	</a-marker>
+    	<a-entity camera></a-entity>
+    </a-scene>
   </div>
 </template>
 
